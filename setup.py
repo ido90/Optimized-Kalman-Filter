ba21ca7f61590f571b3f1c8cf930a2ae6d99203f
ido90 <idogreenberg90@gmail.com>
2022-06-13 13:41:28 +0300

    v0.0.3: minor preprocessing check bugfix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Optimized Kalman Filter",
-    version="0.0.2",
+    version="0.0.3",
     license='MIT',
     author="Ido Greenberg",
     description="Optimization of a Kalman Filter from data of states and their observations.",
